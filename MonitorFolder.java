3af25c7e0a289d43810be17d7adfaa964806e745
Artowod <myjavaname@gmail.com>
2018-04-20 23:44:43 +0300

    5th task is DONE

--- MonitorFolder.java
+++ MonitorFolder.java
@@ -13,54 +13,35 @@ public class MonitorFolder implements Runnable {
 
 	}
 
-//	private File checkWhatWasChanged(File initialFolderState, File currentFolderState) {
-//		for(String x: initialFolderState.list()) {
-//			System.out.print(x + " ");
-//
-//		}
-//		System.out.println();
-//		
-//		for(String y: currentFolderState.list()) {
-//			System.out.print(y + " ");
-//
-//		}		
-//		if (initialFolderState.list().length > currentFolderState.list().length) {
-//			for(String fileCounter:initialFolderState.list() ) {
-//				if(!currentFolderState.list().toString().contains(fileCounter)) {
-//					System.out.println("--del--");
-//					return new File(fileCounter);
-//				}
-//			}
-//		} 		
-//		if (currentFolderState.list().length > initialFolderState.list().length) {
-//			for(String fileCounter:currentFolderState.list() ) {
-//				if(!initialFolderState.list().toString().contains(fileCounter)) {
-//					System.out.println("--add--");
-//					return new File(fileCounter);
-//				}
-//			}
-//		} 
-//		return new File("C:\\temp\\My USB\\Falcon\\mf1.jpg"); /* will be changed */
-//	}
+	private void checkWhatWasChanged(String[] initialFolderState, String[] currentFolderState) {
+		if (initialFolderState.length > currentFolderState.length) {
+			for (String fileCounterLongerFilesList : initialFolderState) {
+				if (!String.join("", currentFolderState).contains(fileCounterLongerFilesList)) {
+					System.out.println(fileCounterLongerFilesList + " has been deleted.");
+					break;
+				}
+			}
+		}
+		if (currentFolderState.length > initialFolderState.length) {
+			for (String fileCounterLongerFilesList : currentFolderState) {
+				if (!String.join("", initialFolderState).contains(fileCounterLongerFilesList)) {
+					System.out.println(fileCounterLongerFilesList + " has been added.");
+					break;
+				}
+			}
+		}
+	}
 
 	@Override
 	public void run() {
 		File monitoredFolder = new File(SourceFolderPatch);
+		String[] initialFilesList = monitoredFolder.list();
 		for (int i = 0; i < 100; i++) {
-			File currentMonitoredFolder = new File(SourceFolderPatch);
-			
-			System.out.print("initial files list: < ");
-			for(String x: monitoredFolder.list()) {
-				System.out.print(x + " ");
+			String[] currentFilesList = monitoredFolder.list();
+			if (initialFilesList.length != currentFilesList.length) {
+				checkWhatWasChanged(initialFilesList, currentFilesList);
+				initialFilesList = currentFilesList;
 			}
-			System.out.println(">");
-			
-			System.out.print("current files list: < ");
-			for(String y: currentMonitoredFolder.list()) {
-				System.out.print(y + " ");
-			}
-			System.out.println(">\n--");
-			
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
@@ -68,19 +49,6 @@ public class MonitorFolder implements Runnable {
 			}
 		}
 
-	}			
-			
-//			System.out.println("<" + monitoredFolderLength + "," + currentMonitoredFolderLength + ">");
-//			if (monitoredFolderLength < currentMonitoredFolderLength) {
-//				System.out.println("\n Some file or folder has been added to the monitored folder.");
-//				File changedObject = checkWhatWasChanged(newMonitoredFolder,currentMonitoredFolder);
-//				System.out.println(changedObject.getName() + " has been added.");
-			//	monitoredFolderLength = currentMonitoredFolderLength;
-//			}
-//			if (monitoredFolderLength > currentMonitoredFolderLength) {
-//				System.out.println("\n Some file or folder has been deleted from the monitored folder.");
-//				File changedObject = checkWhatWasChanged(newMonitoredFolder,currentMonitoredFolder);
-//				System.out.println(changedObject.getName() + " has been deleted.");
-			//	monitoredFolderLength = currentMonitoredFolderLength;
-//			}
+	}
+
 }
